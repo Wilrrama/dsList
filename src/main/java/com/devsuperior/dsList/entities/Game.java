fd7295f52e5d203a62b7a08d09d213ec31532a33
wilrrama <wilrrama@hotmail.com>
2023-05-19 13:49:06 -0300

    seed games

--- src/main/java/com/devsuperior/dsList/entities/Game.java
+++ src/main/java/com/devsuperior/dsList/entities/Game.java
@@ -17,23 +17,29 @@ public class Game {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
-	@Column(name = "game_years")
+	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl,
 			String shortDescription, String longDescription) {		
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score= score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -71,12 +77,22 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+	
+		
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
@@ -105,7 +121,7 @@ public class Game {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(genre, id, imgUrl, longDescription, platform, shortDescription, title, year);
+		return Objects.hash(genre, id, imgUrl, longDescription, platforms, score, shortDescription, title, year);
 	}
 
 	@Override
@@ -119,11 +135,9 @@ public class Game {
 		Game other = (Game) obj;
 		return Objects.equals(genre, other.genre) && Objects.equals(id, other.id)
 				&& Objects.equals(imgUrl, other.imgUrl) && Objects.equals(longDescription, other.longDescription)
-				&& Objects.equals(platform, other.platform) && Objects.equals(shortDescription, other.shortDescription)
-				&& Objects.equals(title, other.title) && Objects.equals(year, other.year);
-	}
-	
-	
-	
+				&& Objects.equals(platforms, other.platforms) && Objects.equals(score, other.score)
+				&& Objects.equals(shortDescription, other.shortDescription) && Objects.equals(title, other.title)
+				&& Objects.equals(year, other.year);
+	}	
 	
 }
